42f604affabbcb3b5435731dbc75618ac09f8968
gdickson52 <gdickson52@gmail.com>
2019-10-23 13:34:19 -0400

    updated

--- word_frequency.py
+++ word_frequency.py
@@ -46,5 +46,5 @@ if __name__ == "__main__":
     if file.is_file():
         print_word_freq(file)
     else:
-        print(f"{file} does not exist!")
+        print(f"{file} sorry, does not exist!")
         exit(1)
